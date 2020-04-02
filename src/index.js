16313a445afe4af84045b14e9453816a7a76dae7
asengelhart <asengelhart@yahoo.com>
2020-04-01 18:22:26 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+const form = document.getElementById('create-task-form');
+const field = document.getElementById('new-task-description');
+const tasks = document.getElementById('tasks');
+
+function getNewTask() {
+  const taskDesc = field.value;
+  console.log("Task listed as: ", taskDesc);
+  return taskDesc;
+}
+
+function addTask(taskDesc) {
+  newTask = document.createElement('li');
+  newTask.innerText = taskDesc;
+  tasks.appendChild(newTask);
+}
+
+function submitTask(e) {
+  e.preventDefault();
+  const taskDesc = getNewTask();
+  addTask(taskDesc);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener('submit', submitTask);
 });
